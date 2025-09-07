f6fd25437ec8a09196cfbfeae8b3c61442577912
SaydurRahmanSaied <saydurrahman3952@gmail.com>
2025-09-07 14:08:18 +0600

    Learn local Module

---
+++ Module.js
@@ -0,0 +1,15 @@
+// ArrowFunction
+
+const getName = () =>{
+    return "Your Name & Age: ";
+}
+
+const getAge = () => {
+    return "20";
+}
+
+export const name = getName;   
+export const age = getAge;
+
+//access another file
+
---
+++ index.js
@@ -0,0 +1,6 @@
+const s1 = require("./Module");
+
+// console.log(s1.name(), s1.age());
+
+const {} = require('./Module');
+console.log(s1.name());
